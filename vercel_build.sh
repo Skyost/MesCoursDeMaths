f64a6bfbfe324ee83ded702d8ef9351f7b04c32a
Hugo Delaunay <Skyost@users.noreply.github.com>
2022-05-30 21:27:48 +0200

    Passage de TeX Live à MiKTeX.

--- vercel_build.sh
+++ vercel_build.sh
@@ -19,7 +19,10 @@ yum install curl -y
 wget -q http://springdale.math.ias.edu/data/puias/unsupported/7/x86_64/dnf-conf-0.6.4-2.sdl7.noarch.rpm
 wget -q http://springdale.math.ias.edu/data/puias/unsupported/7/x86_64//dnf-0.6.4-2.sdl7.noarch.rpm
 wget -q http://springdale.math.ias.edu/data/puias/unsupported/7/x86_64/python-dnf-0.6.4-2.sdl7.noarch.rpm
-yum install python-dnf-0.6.4-2.sdl7.noarch.rpm  dnf-0.6.4-2.sdl7.noarch.rpm dnf-conf-0.6.4-2.sdl7.noarch.rpm
+yum install libreport-filesystem -y
+yum install python-dnf-0.6.4-2.sdl7.noarch.rpm -y
+yum install dnf-0.6.4-2.sdl7.noarch.rpm -y
+yum install dnf-conf-0.6.4-2.sdl7.noarch.rpm -y
 rpm --import "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xD6BC243565B2087BC3F897C9277A7293F59E4889"
 curl -L -o /etc/yum.repos.d/miktex.repo https://miktex.org/download/centos/8/miktex.repo
 dnf update
