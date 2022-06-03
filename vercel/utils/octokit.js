c046ee5e14c21c0bd071ed46ab96a860fdf20955
Hugo Delaunay <Skyost@users.noreply.github.com>
2022-06-03 22:18:41 +0200

    Des corrections.

--- vercel/utils/octokit.js
+++ vercel/utils/octokit.js
@@ -4,8 +4,12 @@ const cookie = require('cookie')
 const { Octokit } = require('@octokit/core')
 
 function createOctokitFromRequest (request, response) {
+  if (!Object.prototype.hasOwnProperty.call(request.headers, 'cookie')) {
+    response.status(401).send('Pas de jeton d\'accès.')
+    return null
+  }
   const cookies = cookie.parse(request.headers.cookie)
-  if (!('access_token' in cookies)) {
+  if (!Object.prototype.hasOwnProperty.call(cookies, 'access_token')) {
     response.status(401).send('Pas de jeton d\'accès.')
     return null
   }
