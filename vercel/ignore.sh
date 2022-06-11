95375c38b0a8d878d6a2b148f577cc5553a138f5
Hugo Delaunay <Skyost@users.noreply.github.com>
2022-06-11 17:51:41 +0200

    Mise à jour du script `ignore.sh`.

--- vercel/ignore.sh
+++ vercel/ignore.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "master"  ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "master" ]] ; then
   # Proceed with the build
-  echo "✅ Ref is `master` !"
+  echo "✅ Ref is 'master'. The build can proceed !"
   exit 1;
 
 else
   # Don't build
-  echo "🛑 Ref is not `master`. Cancelling build..."
+  echo "🛑 Ref is not 'master'. Cancelling build..."
   exit 0;
 fi
