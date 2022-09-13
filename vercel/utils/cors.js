94fe51521cfe16ee992a691aca436cce514b8e4f
Hugo Delaunay <Skyost@users.noreply.github.com>
2022-09-13 18:53:08 +0200

    Mise à jour des en-têtes CORS.

--- vercel/utils/cors.js
+++ vercel/utils/cors.js
@@ -3,7 +3,7 @@ import siteMeta from '../../site/meta'
 function allowCors (response) {
   response.setHeader('Access-Control-Allow-Credentials', true)
   response.setHeader('Access-Control-Allow-Origin', siteMeta.url)
-  response.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT')
+  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   response.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, Origin'
