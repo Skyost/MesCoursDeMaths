3a95b8720d1bbd1607fef762f99f4a93544fc971
imbodj <91032893+imbodj@users.noreply.github.com>
2022-09-13 21:42:23 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div>
+    <page-navbar />
+    <page-header />
+    <page-content>
+      <slot />
+    </page-content>
+    <page-footer />
+   
+  </div>
 </template>
+
+<script>
+import PageNavbar from '~/components/Page/PageNavbar'
+import PageContent from '~/components/Page/PageContent'
+import PageFooter from '~/components/Page/PageFooter'
+import PageHeader from '~/components/Page/PageHeader'
+export default {
+  components: { PageHeader, PageFooter, PageContent, PageNavbar }
+}
+</script>
+Footer
+
