793f0db2e18a5e2a7eab9547ed7a88b122362826
imbodj <91032893+imbodj@users.noreply.github.com>
2022-09-16 20:40:07 +0100

    Update meta.js

--- site/meta.js
+++ site/meta.js
@@ -2,7 +2,7 @@ export default {
   title: 'Sen cours de maths',
   description: 'Petit site web où je mets tous mes cours à disposition ainsi que les ressources qui y sont liés.',
   author: 'Ismaila Mbodji',
-  url: 'https://sen-cours-de-maths.vercel.app',
+  url: 'https://sencoursdemaths.com/',
   apiUrl: 'https://sen-cours-de-maths.vercel.app',
   github: {
     username: 'imbodji',
