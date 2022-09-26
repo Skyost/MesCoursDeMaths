273e00977fd3fa4308ebb4337e523817ab9a2745
Hugo Delaunay <Skyost@users.noreply.github.com>
2022-09-26 15:12:24 +0200

    Une correction.

--- site/meta.js
+++ site/meta.js
@@ -1,6 +1,6 @@
 export default {
   title: 'Mes cours de maths',
-  description: 'Petit site web où je mets tous mes cours à disposition ainsi que les ressources qui y sont liés.',
+  description: 'Petit site web où je mets tous mes cours à disposition ainsi que les ressources qui y sont liées.',
   author: 'Hugo Delaunay',
   url: 'https://mes-cours-de-maths.fr',
   apiUrl: 'https://vercel.mes-cours-de-maths.fr/api',
