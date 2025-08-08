925bcf58f427b73630b8ececff90ad01746ff429
Hugo Delaunay <Skyost@users.noreply.github.com>
2025-08-08 18:08:08 +0200

    Ajout de `data-bs-theme="dark"` à la navbar (pour éviter que ce soit noir au chargement de la page).

--- app/components/Page/PageNavbar.vue
+++ app/components/Page/PageNavbar.vue
@@ -6,6 +6,7 @@ const theme = useTheme()
   <b-navbar
     class="page-navbar"
     v-b-color-mode="'dark'"
+    data-bs-theme="dark"
     toggleable="lg"
     :variant="theme === 'light' ? 'primary' : 'black'"
   >
